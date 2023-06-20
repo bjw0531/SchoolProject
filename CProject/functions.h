9827ffd389dc19ddd34819b38d1e3c17eb0d8062
방지원 <bjw0531@naver.com>
2023-06-20 13:32:31 +0900

    update

--- CProject/functions.h
+++ CProject/functions.h
@@ -1,37 +1,72 @@
 #pragma once
 #include "preprocess.h"
 
-void VaryPrice() // 스레드 함수
+
+void gotoxy(int x, int y) // 콘솔 마우스 커서 이동 함수
 {
-    // 이현웅 담당
+    COORD Pos = { x * 2, y };   // 인자를 COORD 자료형으로 변경, x의 좌표는 2배를 취해야 1:1 비율이기 때문
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos); // WinCon.h에 정의된 함수로 커서 위치를 변경, GetStdHandle로 출력창의 핸들을 가져옴
+}
+
+void ColorPrint(double num)
+{
+    char tmp[100] = { 0, };
+    if (num > 0)  printf("\033[38;2;242;61;61m+ ");  // 빨간색으로 색을 변경, +출력
+    if (num < 0)  printf("\033[38;2;38;145;204m- ");   // 파란색으로 색을 변경, -출력
+    printf("%.1lf%%", fabs(num));
+    printf("\033[0m");
+}
+
+void WalletPrint(ll wallet[], int x, int y) // 지갑 상태출력
+{
+    int cnt = 0;
+    gotoxy(x + cnt * 13, y-2);
+    printf("현금 : $%.2lf", mycash);  // 현재 돈 출력
 
     for (int i = 0; i < CRYPTOCNT; i++)
     {
-        float randomChange = float(((rand() % 40) + 10)) / 1000;
-        // 50%의 확률로 상승 또는 하락    
-        Crypto[i].price = Crypto[i].price * (1 + randomChange);
-        if (rand() % 2 == 0) {
-            Crypto[i].price += randomChange * Crypto[i].price;  // 퍼센트 값을 증감
+        if (wallet[i] > 0)  // wallet[i] = i번째 가상화폐 갯수
+        {
+            gotoxy(x + cnt * 13, y);
+            printf("이름 : %s\n", Crypto[i].name);    // i번째 가상화폐 이름
+            gotoxy(x + cnt * 13, y + 1);
+            printf("갯수 : %lld\n", wallet[i]);       // i번째 갯수 출력
+            gotoxy(x + cnt++ * 13, y + 2);
+            printf("총액 : $%.2lf", wallet[i] * Crypto[i].price); // 가격 * 현재개수
         }
-        else {
-            Crypto[i].price -= randomChange * Crypto[i].price;  // 퍼센트 값을 증감
+    }
+}
+
+void VaryPrice()
+{
+    for (int i = 0; i < CRYPTOCNT; i++) // 모든 가상화폐에 대해서
+    {
+        srand(time(NULL) + i);    // 현재시간+i로 rand 시드 초기화
+        double randomChange = double(rand() % 70 + 1) / 1000;   // 0.001-0.07 : 0.1프로에서 7프로까지
+        Crypto[i].updown = randomChange * 100;  // i번째 가상화폐의 등락폭 저장
+        if (rand() % 100 > 46)  // 0~46 까지 하락, 47~99까지 상승
+        {
+            Crypto[i].price += randomChange * Crypto[i].price;  // {randomChange}% 만큼 상승
+        }
+        else 
+        {
+            Crypto[i].price -= randomChange * Crypto[i].price;  // {randomChange}% 만큼 하락
+            Crypto[i].updown *= -1;
         }
     }
     
 }
 
-void gotoxy(int x, int y) // 콘솔 마우스 커서 이동 함수
-{
-    COORD Pos = { x * 2, y };
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
-}
 
 void CCprint(struct _CrypCur* CC, int x, int y) // CC 출력 함수
 {
     gotoxy(x, y);
-    printf("이름 : %s", CC->name);
+    printf("이름 : %s", CC->name);    // 이름 출력
     gotoxy(x, y + 1);
-    printf("가격 : $%.2lf", CC->price);
+    printf("가격 : $%.2lf", CC->price);   // 가격 출력
+    gotoxy(x, y + 2);
+    printf("등락폭 : ");   // 등락폭 출력
+    ColorPrint(CC->updown);
 }
 
 void CCinit(struct _CrypCur* CC, const char name[], double price, int idx) // CC 초기화 함수
@@ -39,13 +74,7 @@ void CCinit(struct _CrypCur* CC, const char name[], double price, int idx) // CC
     CC->idx = idx;
     strcpy(CC->name, name);
     CC->price = price;
-}
-
-void PrtScrHead() 
-{
-    for (int i = DASHCNT; i--;)  printf("─");
-    printf("Virtual CryptoCurrency Market");
-    for (int i = DASHCNT; i--;)  printf("─");
+    CC->updown = 0;
 }
 
 void Asciitxt()
@@ -74,7 +103,7 @@ void Asciitxt()
 void customPause(const char* message) // 사용자 지정 pause
 {
     printf("%s", message);
-    getch(); 
+    _getch(); 
 }
 
 void InitScreen() // 초기 시작 화면
@@ -95,21 +124,30 @@ void PrtCCInfo(int x, int y) // CC 출력
     gotoxy(x, y);
     for (int i = 0; i < CRYPTOCNT; i++) CCprint(&Crypto[i], i * 13, y);
     gotoxy((CRYPTOCNT - 1) * 13, y + 4);
-    printf("잔고 : $%.2lf", mycash);
+
+    WalletPrint(wallet, x, y + 20);
 }
 
 void CursorView() // 커서 가리기
 {
-    CONSOLE_CURSOR_INFO cursorInfo = { 0, };
-    cursorInfo.dwSize = 1; //커서 굵기 (1 ~ 100)
-    cursorInfo.bVisible = FALSE; //커서 Visible TRUE(보임) FALSE(숨김)
-    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+    CONSOLE_CURSOR_INFO cursorInfo = { 0, };    // CONSOLE_CURSOR_INFO 자료형
+    cursorInfo.dwSize = 1; // 커서 굵기 (1 ~ 100)
+    cursorInfo.bVisible = FALSE; // 커서 Visible TRUE(보임) FALSE(숨김)
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo); // Handle의 Cursor정보를 위의 정보로 변경
 }
 
 void ApplyTransaction(int mode, int idx, int cnt)
 {
+    int type = 0;
+    if (mode == 1)      type = -1;  // 구매이면 -1
+    else if (mode == 2) type = 1;   // 판매이면 1
 
-}
+    mycash += Crypto[idx].price * cnt * type;   // 지갑에 돈을 넣을지 뺄지 결정함
+    
+    type *= -1; // 구매이면 1, 판매이면 -1이 됨
+    wallet[idx] += cnt * type;
+
+}   
 
 void StartTransaction(int ccprintpos, int printpos)
 {
@@ -118,7 +156,7 @@ select1:
     system("cls");
     PrtCCInfo(0, ccprintpos);
     gotoxy(0, printpos);
-    printf("(1) 구매\n(2) 판매");
+    printf("(1) 구매\n(2) 판매\n(ESC) 돌아가기");
 
     while (1)
     {
@@ -137,6 +175,7 @@ select2:
     PrtCCInfo(0, ccprintpos);
     gotoxy(0, printpos);
     for (int i = 0; i < CRYPTOCNT; i++) printf("(%d) %s\n", i + 1, Crypto[i].name);
+    printf("(ESC) 돌아가기\n");
 
     while (1)
     {
@@ -153,11 +192,19 @@ select3:
         PrtCCInfo(0, ccprintpos);
         gotoxy(0, printpos);
         printf("갯수: ");
-        scanf("%d", &CCcnt);
-        if (Crypto[CCidx].price * CCcnt > mycash)
+        scanf("%d", &CCcnt); 
+        if (CCmode == 1 && Crypto[CCidx].price * CCcnt > mycash)    // 구매 거래이고 지갑에 충분한 돈이 없을 경우
         {
             gotoxy(0, printpos+1);
             printf("잔액이 부족합니다.\n");
+            Sleep(1000);
+            system("cls");
+            continue;
+        }
+        if (CCmode == 2 && wallet[CCidx] < CCcnt)   // 판매 거래이고 지갑에 충분한 가상화폐가 없을 경우
+        {
+            gotoxy(0, printpos + 1);
+            printf("가상화폐가 부족합니다.\n");
             Sleep(1000);
             system("cls");
             continue;
@@ -169,5 +216,8 @@ select3:
         Sleep(1000);
         break;
     }
+    ApplyTransaction(CCmode, CCidx, CCcnt);
+
     system("cls");
 }
+
--- CProject/preprocess.h
+++ CProject/preprocess.h
@@ -16,10 +16,12 @@ struct _CrypCur // 가상화폐 구조체
 {
     char name[50];
     double price;
+    double updown;
     int idx;
 } Crypto[CRYPTOCNT];
 
-char names[CRYPTOCNT][50] = { "Apple", "Kakao", "Samsung", "Tesla", "Microsoft" };
-ll prices[CRYPTOCNT] = { 100,200,300,400,5000000 };
+char names[CRYPTOCNT][50] = { "DOGE", "USDT", "XRP", "ETH", "BTC" };
+double prices[CRYPTOCNT] = { 5,50,200,1500,25000 };
 ll wallet[CRYPTOCNT] = { 0, };
+double lastpercent[CRYPTOCNT] = { 0, };
 double mycash = 10000000;
