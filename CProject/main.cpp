1a3b538dd88b25f2471330840e0d9e13ceaaf5da
방지원 <31693657+bjw0531@users.noreply.github.com>
2023-06-22 13:46:08 +0900

    Update main.cpp

--- CProject/main.cpp
+++ CProject/main.cpp
@@ -1,4 +1,4 @@
-﻿#include "preprocess.h"
+#include "preprocess.h"
 #include "functions.h"
 
 int age = 0;
@@ -12,6 +12,7 @@ int main()
 
     while (1)
     {
+        CursorView();
         clock_t now = clock();
         int currentage = int(now - start) / CLOCKS_PER_SEC / 2;
         if (age != currentage)
@@ -26,7 +27,5 @@ int main()
 
         if (GetAsyncKeyState(VK_RETURN) < 0)    StartTransaction(1, 5);
         else                                    printf("동작하려면 엔터를 누르세요.");
-
     }
-    
 }	
